e436a1d5d385df235a1a2600e93681225b4b0f7e
Mark <45356695+z-Phoenix-z@users.noreply.github.com>
2018-12-17 19:29:57 +0800

    Update BucketSort.java

--- BucketSort.java
+++ BucketSort.java
@@ -5,13 +5,12 @@
 
 int [] a = new int [2,3,4,5,2,7];
 int [] b = new int [7+1];               //长度等于a的最大值，看条件写不写找maximum的方法。 最大值要➕1；
-int [] temp = new int [];
 for (int i = 0; i < a.length ; i ++){
  b [a[i]] ++;
 }
 for (int i = 0; i < b.length; i ++){
   if (b [i] != 0){
-    return i;
+    System.out.print( b[i] );
   }
  
 
